7a440d467d43e2d57fdad08c94b174cc69f31f8a
PK-git27 <Pk5501523>
2020-09-25 17:03:19 +0800

    公共页面代码提取

--- cloud-index8002/src/main/java/com/yc/cloudindex8002/controller/indexController.java
+++ cloud-index8002/src/main/java/com/yc/cloudindex8002/controller/indexController.java
@@ -6,9 +6,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class indexController {
     @RequestMapping(path={"/404.html","404"})
-    public String index(){
+    public String s404(){
         return "404";
     }
     @RequestMapping(path={"login.html","login"})
     public String login() { return "login"; }
+    @RequestMapping(path={"shop.html","shop"})
+    public String shop() { return "shop"; }
+    @RequestMapping(path={"index.html","index"})
+    public String index() { return "index"; }
 }
