be664e81177959ad74647e5b01a6489235036e85
Khao-thin <2316377605@qq.com>
2020-09-25 17:10:33 +0800

    添加common工程0.1

--- cloud-index8002/src/main/java/com/yc/cloudindex8002/controller/indexController.java
+++ cloud-index8002/src/main/java/com/yc/cloudindex8002/controller/indexController.java
@@ -1,18 +1,18 @@
 package com.yc.cloudindex8002.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class indexController {
-    @RequestMapping(path={"/404.html","404"})
-    public String s404(){
-        return "404";
+    @RequestMapping(path={"login.html"})
+    public String toLogin() { return "login"; }
+    @PostMapping("login")
+    public String login(String username,String password){
+        return "index";
     }
-    @RequestMapping(path={"login.html","login"})
-    public String login() { return "login"; }
     @RequestMapping(path={"shop.html","shop"})
     public String shop() { return "shop"; }
-    @RequestMapping(path={"index.html","index"})
-    public String index() { return "index"; }
+
 }
---
+++ cloud-index8002/src/main/java/com/yc/cloudindex8002/mapper/loginMapper.java
@@ -0,0 +1,8 @@
+package com.yc.cloudindex8002.mapper;
+
+import org.apache.ibatis.annotations.Select;
+
+public interface loginMapper {
+    //@Select("select * from an_user")
+
+}
