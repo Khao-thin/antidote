a5861d7de7ddf54adf964e7c9a0d3aa32758a37a
Khao-thin <2316377605@qq.com>
2020-09-25 09:04:35 +0800

    test0.1

--- cloud-index8002/src/main/java/com/yc/cloudindex8002/controller/indexController.java
+++ cloud-index8002/src/main/java/com/yc/cloudindex8002/controller/indexController.java
@@ -1,4 +1,12 @@
 package com.yc.cloudindex8002.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class indexController {
+    @RequestMapping(path={"/404.html","404"})
+    public String index(){
+        return "404";
+    }
 }
