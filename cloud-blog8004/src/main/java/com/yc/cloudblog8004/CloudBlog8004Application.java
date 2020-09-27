e2802847d6f8052b99bcc2e88c496164fe655ba0
Khao-thin <2316377605@qq.com>
2020-09-27 09:54:00 +0800

    blog工程提交

---
+++ cloud-blog8004/src/main/java/com/yc/cloudblog8004/CloudBlog8004Application.java
@@ -0,0 +1,13 @@
+package com.yc.cloudblog8004;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudBlog8004Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudBlog8004Application.class, args);
+    }
+
+}
---
+++ cloud-blog8004/src/test/java/com/yc/cloudblog8004/CloudBlog8004ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yc.cloudblog8004;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudBlog8004ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
