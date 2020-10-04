0cc9d369f62b1f61dd878b56816cd616ae95a16e
PK-git27 <Pk5501523>
2020-10-05 00:05:15 +0800

    shop渲染和分页

--- cloud-product8001/src/main/java/com/yc/antidote/CloudProduct8001Application.java
+++ cloud-product8001/src/main/java/com/yc/antidote/CloudProduct8001Application.java
@@ -1,9 +1,18 @@
 package com.yc.antidote;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableEurekaClient
+//服务降级注解
+@EnableCircuitBreaker
 @SpringBootApplication
+@MapperScan("com.yc.antidote.dao")
+@EnableFeignClients
 public class CloudProduct8001Application {
 
     public static void main(String[] args) {
