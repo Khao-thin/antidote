71f159cdd832b7e1041dd6cb95ab224b8ff96aab
Khao-thin <2316377605@qq.com>
2020-10-05 00:02:26 +0800

    登录0.1

--- cloud-user8003/src/main/java/com/yc/antidote/CloudUser8003Application.java
+++ cloud-user8003/src/main/java/com/yc/antidote/CloudUser8003Application.java
@@ -5,11 +5,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @EnableEurekaClient
 // 服务降级注解
 @EnableCircuitBreaker
 @SpringBootApplication
+// 开启Redis会话共享
+@EnableRedisHttpSession
 @MapperScan("com.yc.antidote.dao")
 public class CloudUser8003Application {
 
